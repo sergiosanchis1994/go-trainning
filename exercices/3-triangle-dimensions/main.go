9d6a44e88b51ee39348332945f6b741526e91e95
Sergio Sanchis <sergiosanchis1994@gmail.com>
2024-04-28 17:03:56 +0200

    refactor hypotenuse operation

--- exercices/3-triangle-dimensions/main.go
+++ exercices/3-triangle-dimensions/main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Print("Enter side 2: ")
 	fmt.Scanln(&side2)
 
-	hypotenuse := math.Sqrt(side1*side1 + side2*side2)
+	hypotenuse := math.Sqrt(math.Pow(side1, 2) + math.Pow(side2, 2))
 	area := (side1 * side2) / 2
 	perimeter := side1 + side2 + hypotenuse
 
