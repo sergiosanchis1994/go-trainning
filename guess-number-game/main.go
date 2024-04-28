3687d1c76a3269b9c36bdf72b7f049c8a8ae7af9
Sergio Sanchis <sergiosanchis1994@gmail.com>
2024-04-28 18:34:03 +0200

    switch

--- guess-number-game/main.go
+++ guess-number-game/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
-	if t := time.Now(); t.Hour() < 12 {
+	switch t := time.Now(); {
+	case t.Hour() < 12:
 		fmt.Println("Morning")
-	} else if t.Hour() < 21 {
+	case t.Hour() < 21:
 		fmt.Println("Afternoon")
-	} else {
+	default:
 		fmt.Println("Night")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "windows":
+		fmt.Println("Go run -> Windows")
+	case "linux":
+		fmt.Println("Go run -> Linux")
+	case "darwin":
+		fmt.Println("Go run -> macOS")
+	default:
+		fmt.Println("Go run -> Unknown")
 	}
 }
