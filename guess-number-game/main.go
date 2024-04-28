09dd3279c2acbad83b85cb7f9db7c7068a2f9ce7
Sergio Sanchis <sergiosanchis1994@gmail.com>
2024-04-28 19:06:13 +0200

    game implemented

--- guess-number-game/main.go
+++ guess-number-game/main.go
@@ -1,20 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	fmt.Println(hello("Sergio"))
-	sum, prod := calc(4, 5)
-	fmt.Println("Sum:", sum)
-	fmt.Println("Prod:", prod)
+	play()
 }
 
-func hello(name string) string {
-	return "Hello " + name
+func play() {
+	randomNumber := rand.Intn(100)
+	var num int
+	var tries int
+	const maxTries = 10
+
+	for tries < maxTries {
+		tries++
+		fmt.Printf("Introduce a number (tries: %d): ", maxTries-tries+1)
+		fmt.Scanln(&num)
+
+		if num == randomNumber {
+			fmt.Printf("Congrats! %d was the secret number!\n", num)
+			newGame()
+			return
+		} else if num < randomNumber {
+			fmt.Println("Your number is lower than the secret number")
+		} else if num > randomNumber {
+			fmt.Println("Your number is bigger than the secret number")
+		}
+	}
+	fmt.Printf("You lost :( Secret number was: %d \n", randomNumber)
+	newGame()
 }
 
-func calc(a, b int) (sum, prod int) {
-	sum = a + b
-	prod = a * b
-	return
+func newGame() {
+	var choice string
+	fmt.Printf("Do you want to play again? (y/n): ")
+	fmt.Scanln(&choice)
+	switch choice {
+	case "y":
+		play()
+	case "n":
+		fmt.Println("Bye :)")
+	default:
+		fmt.Println("Invalid option. Please, introduce only \"y\" or \"n\"")
+		newGame()
+	}
 }
