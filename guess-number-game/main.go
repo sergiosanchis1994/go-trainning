6c5cdce1871cc76f7a3a5e4ca5217bf74e2d8c68
Sergio Sanchis <sergiosanchis1994@gmail.com>
2024-04-28 18:46:35 +0200

    func with return values in method

--- guess-number-game/main.go
+++ guess-number-game/main.go
@@ -13,8 +13,8 @@ func hello(name string) string {
 	return "Hello " + name
 }
 
-func calc(a, b int) (int, int) {
-	sum := a + b
-	prod := a * b
-	return sum, prod
+func calc(a, b int) (sum, prod int) {
+	sum = a + b
+	prod = a * b
+	return
 }
