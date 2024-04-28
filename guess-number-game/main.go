60b5d1617d748197c796266838edf2d8704a8ab4
Sergio Sanchis <sergiosanchis1994@gmail.com>
2024-04-28 18:45:43 +0200

    func return more than one value

--- guess-number-game/main.go
+++ guess-number-game/main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	hello("Sergio")
+	fmt.Println(hello("Sergio"))
+	sum, prod := calc(4, 5)
+	fmt.Println("Sum:", sum)
+	fmt.Println("Prod:", prod)
 }
 
-func hello(name string) {
-	fmt.Println("Hello,", name)
+func hello(name string) string {
+	return "Hello " + name
+}
+
+func calc(a, b int) (int, int) {
+	sum := a + b
+	prod := a * b
+	return sum, prod
 }
