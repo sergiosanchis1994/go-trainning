875cf845e65ec1a179a6dd8c184fbbd869900bce
Sergio Sanchis <sergiosanchis1994@gmail.com>
2024-04-28 13:53:23 +0200

    type conversion

--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"rsc.io/quote"
 )
@@ -9,13 +10,17 @@ import (
 func main() {
 	fmt.Println(quote.Hello())
 
-	var (
-		defaultInt    int
-		defaultUint   uint
-		defaultFloat  float32
-		defaultBool   bool
-		defaultString string
-	)
+	var integer16 int16 = 50
+	var integer32 int32 = 100
 
-	fmt.Println(defaultInt, defaultUint, defaultFloat, defaultBool, defaultString)
+	fmt.Println(integer16 + int16(integer32)) //150
+
+	s := "100"
+	i, _ := strconv.Atoi(s)
+	fmt.Println(i + i) //200
+
+	n := 42
+	s = strconv.Itoa(n)
+	fmt.Println(s + s) //4242
+
 }
