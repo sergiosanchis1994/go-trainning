cefa742ad339164328542dbafe62b806e8b7562a
Sergio Sanchis <sergiosanchis1994@gmail.com>
2024-04-28 12:25:15 +0200

    add variables

--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -7,6 +7,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
-	fmt.Println(quote.Go())
+
+	var (
+		helloWorldMessage string = quote.Hello()
+		goMessage         string = quote.Go()
+	)
+
+	optMessage := quote.Opt()
+
+	fmt.Println(helloWorldMessage)
+	fmt.Println(goMessage)
+	fmt.Println(optMessage)
 }
