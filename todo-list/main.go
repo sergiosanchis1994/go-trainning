527e8331f533f6028e3373df8936136c1a2023d5
Sergio Sanchis <sergiosanchis1994@gmail.com>
2024-05-01 17:38:45 +0200

    slices copy

--- todo-list/main.go
+++ todo-list/main.go
@@ -5,7 +5,13 @@ import (
 )
 
 func main() {
-	names := make([]string, 5, 10)
-	names[0] = "Sergio"
-	fmt.Println(names)
+	slice1 := []int{1, 2, 3, 4, 5}
+	slice2 := make([]int, 5)
+
+	//target source
+	elements := copy(slice2, slice1)
+
+	fmt.Println("Elements:", elements)
+	fmt.Println(slice1)
+	fmt.Println(slice2)
 }
