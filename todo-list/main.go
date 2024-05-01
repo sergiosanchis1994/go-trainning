b96db1e4ee54cc81db7959bbce4af3356940b0d9
Sergio Sanchis <sergiosanchis1994@gmail.com>
2024-05-01 13:08:18 +0200

    slices make

--- todo-list/main.go
+++ todo-list/main.go
@@ -5,20 +5,7 @@ import (
 )
 
 func main() {
-	week := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
-
-	days := week[0:5]
-
-	fmt.Println(week)
-	fmt.Println(days)
-
-	days = append(days, "Friday")
-	days = append(days, "Saturday")
-	days = append(days, "Extra day")
-	fmt.Println(days)
-
-	days = append(days[:2], days[3:]...)
-	fmt.Println(days)
-	fmt.Println("Length", len(days))
-	fmt.Println("Cap", cap(days))
+	names := make([]string, 5, 10)
+	names[0] = "Sergio"
+	fmt.Println(names)
 }
