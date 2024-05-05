2442ddad871d5ee3cad8f81289c8eaf02fa454bd
Sergio Sanchis <sergiosanchis1994@gmail.com>
2024-05-05 18:09:14 +0200

    panic and recover

--- contacts-management/main.go
+++ contacts-management/main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-func main() {
-	file, err := os.Create("hello.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+func divide(dividend, divisor int) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	validateZero(divisor)
+	fmt.Println(dividend / divisor)
+}
 
-	defer file.Close()
-	_, err = file.Write([]byte("Hello, World!"))
-	if err != nil {
-		fmt.Println(err)
-		return
+func validateZero(divisor int) {
+	if divisor == 0 {
+		panic("You cannot divide by 0")
 	}
+}
+
+func main() {
+	divide(100, 10)
+	divide(200, 25)
+	divide(34, 0)
+	divide(100, 4)
 }
